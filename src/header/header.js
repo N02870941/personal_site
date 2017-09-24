f796338df4b2d9baa5808e4f07e0f0760fc20106
JOD <jod@client148-243.wireless.newpaltz.edu>
2017-09-23 23:34:02 -0400

    more updates

--- src/header/header.js
+++ src/header/header.js
@@ -1,4 +1,36 @@
 app.controller('headerController', function($scope) {
 
 
+  $scope.options = [
+    {
+        name: "home",
+        href: "#!home",
+        text:"Home"
+    },
+    {
+        name: "biography",
+        href: "#!biography",
+        text:"Biography"
+    },
+    {
+        name: "resume",
+        href: "#!resume",
+        text:"Resume"
+    },
+    {
+        name: "projects",
+        href: "#!projects",
+        text:"Tech Projects"
+    },
+    {
+        name: "interests",
+        href: "#!interests",
+        text:"Interests"
+    },
+    {
+        name: "contact",
+        href: "#!contact",
+        text:"Contact"
+    }
+  ];
 });
