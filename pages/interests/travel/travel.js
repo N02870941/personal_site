7d4610166bd170e7444ad94976db175305327608
JOD <jod@client148-243.wireless.newpaltz.edu>
2017-09-24 02:23:37 -0400

    added links to travel

--- pages/interests/travel/travel.js
+++ pages/interests/travel/travel.js
@@ -1,5 +1,25 @@
 app.controller('travelController', function($scope) {
 
+  $scope.countries = [
+    {
+      name: "Colombia",
+      href:'#!/travel/colombia',
+      flag: "/pages/interests/travel/countries/colombia/img/flag_colombia.png",
+      brief: ""
+    },
+    {
+      name: "Ecuador",
+      href: '#!/travel/ecuador',
+      flag: "/pages/interests/travel/countries/ecuador/img/flag_ecuador.png",
+      brief: ""
+    },
+    {
+      name: "Peru",
+      href: '#!/travel/peru',
+      flag: "/pages/interests/travel/countries/peru/img/flag_peru.png",
+      brief: ""
+    }
+  ];
 
 
 });
