8415c26eaef3efaac63ce001f2581c926b0dfa07
Jabari Dash <jabari.dash@gmail.com>
2017-09-25 09:10:01 -0400

    mobile menu

--- client/pages/header/header.js
+++ client/pages/header/header.js
@@ -32,6 +32,17 @@ client.controller('headerController', function($scope) {
         text:"Contact"
     }
   ];
+
+  $scope.myFunction = function() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+  }
+
+
 });
 
 client.directive('header', function() {
