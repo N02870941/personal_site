2783fdcec6e06a9a7262cd82742bf6e24fa954ab
Jabari Dash <jabari.dash@gmail.com>
2017-10-01 14:38:24 -0400

    fix thumbnail retrieve

--- client/pages/interests/photography/photography.service.js
+++ client/pages/interests/photography/photography.service.js
@@ -1,13 +1,13 @@
 app.service('photographyService', function($http, preloader) {
   var protocol = 'http';
-  var domain = 'jabaridash.com';
+  var domain = '127.0.0.1';
   var port = '8080';
 
   function getPhotoPaths() {
     var url = protocol + "://" + domain + ":" + port + "/photography";
 
     return $http.get(url).then(function(res) {
-      
+
       return res.data;
     });
   }
