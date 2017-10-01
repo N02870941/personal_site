868e00c01124c680f74fc8535ef5f5a0bd4e684e
Jabari Dash <jabari.dash@gmail.com>
2017-10-01 04:34:55 -0400

    get photos locally

--- client/pages/interests/photography/photography.service.js
+++ client/pages/interests/photography/photography.service.js
@@ -1,13 +1,13 @@
 app.service('photographyService', function($http, preloader) {
   var protocol = 'http';
-  var domain = 'jabaridash.com';
+  var domain = 'localhost';
   var port = '8080';
 
   function getPhotoPaths() {
     var url = protocol + "://" + domain + ":" + port + "/photography";
 
     return $http.get(url).then(function(res) {
-      
+
       return res.data;
     });
   }
