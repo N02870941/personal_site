35181fb3b257c5e3f90cd7bbf902891fe34d012f
Jabari Dash <jabari.dash@gmail.com>
2017-10-01 07:12:26 -0400

    make thumbnails

--- server/scripts/resize_imgs.sh
+++ server/scripts/resize_imgs.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+create_folder() {
+  # if the bin folder does not exist
+  if [ ! -d $1 ]; then
+    mkdir $1
+  fi
+}
+
+
 # Creates thumbnails by shrinking photo by 25%
 shrink_images() {
-  mogrify -resize 25% -quality 100 -path $2 $1/*.jpg
+  create_folder $2
+  command cd $2
+  command cp $1/* .
+
+  # Max height 400, maintain aspect ration
+  mogrify -geometry x256 -quality 100 -path $2 $1/*.jpg
+
+  mogrify -geometry x2506 -quality 100 -path $2 $1/*.JPG
+
+  mogrify -geometry x256 -quality 100 -path $2 $1/*.png
 }
 
 shrink_images $1 $2
