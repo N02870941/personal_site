60a9073bea67ada76a25281c3fbe8c392ab3f4ad
Jabari Dash <jabari.dash@gmail.com>
2017-10-07 06:17:22 -0400

    route to top for all pages working except travel pages

--- client/pages/projects/projects.controller.js
+++ client/pages/projects/projects.controller.js
@@ -4,31 +4,31 @@ app.controller('projectsController', function($scope) {
   $scope.projects = [
     {
       name: "Discrete and Continuous Algorithms and Data Structures",
-      href:'#!/projects/algorithms',
+      href:'#!/projects/algorithms#top',
       img: "/client/pages/projects/img/thumbnail/algorithms.png",
       brief: "C++ Implementation of common data structures and associate algorithms and interview questions"
     },
     {
       name: "Linear and Nonlinear Regression",
-      href:'#!/projects/regression',
+      href:'#!/projects/regression#top',
       img: "/client/pages/projects/img/thumbnail/regression.png",
       brief: "Basic Python UI that provided a data set displays best fit functions"
     },
     {
       name: "Two Summer Internships at Target",
-      href:'#!/projects/target',
+      href:'#!/projects/target#top',
       img: "/client/pages/projects/img/thumbnail/target.png",
       brief: "Get a peek as to how I spent some of my summers at Target"
     },
     {
       name: "Temperature Collection at SUNY New Paltz",
-      href:'#!/projects/temperature',
+      href:'#!/projects/temperature#top',
       img: "/client/pages/projects/img/thumbnail/temperature.jpg",
       brief: "Distributed Raspberry Pi based project where we collected temperature data and visualized on a website"
     },
     {
       name: "My Personal Website",
-      href:'#!/projects/website',
+      href:'#!/projects/website#top',
       img: "/client/pages/projects/img/thumbnail/website.jpg",
       brief: "This website was completely written by me!"
     }
