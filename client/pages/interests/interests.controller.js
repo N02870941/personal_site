7cbce0cec65da9b94fd1ce62f4b6d1b6920aa8dd
Jabari Dash <jabari.dash@gmail.com>
2017-10-31 22:32:49 -0400

    dont recall changes

--- client/pages/interests/interests.controller.js
+++ client/pages/interests/interests.controller.js
@@ -1,33 +1,30 @@
-app.directive('interests', function() {
-  return {
-    templateUrl: "client/pages/interests/interests.menu.html"
-  }
-});
-
-
 app.controller('interestsController', function($scope) {
 
   $scope.interests = [
     {
-      name: "Travel",
+      title: "Travel",
+      id: 'travel',
       href:'#!/interests/travel#top',
       img: "/client/pages/interests/travel/img/thumbnail/travel.jpg",
       brief: "Check out some of my most recent national and international journies!"
     },
     {
-      name: "Photography",
+      title: "Photography",
+      id: 'photography',
       href:'#!/interests/photography#top',
       img: "/client/pages/interests/photography/img/thumbnail/photography.jpg",
       brief: "See some of my favorite photos that I have taken!"
     },
     {
-      name: "Fitness",
+      title: "Fitness",
+      id: 'fitness',
       href:'#!/interests/fitness#top',
       img: "/client/pages/interests/fitness/img/thumbnail/fitness.jpg",
       brief: "Here are some of my thoughts on fitness and the role it has played in my life."
     },
     {
-      name: "Culture",
+      title: "Culture",
+      id: 'culture',
       href:'#!/interests/culture#top',
       img: "/client/pages/interests/culture/img/thumbnail/culture.jpg",
       brief: "Follow my hair journey and other cultural experiences."
