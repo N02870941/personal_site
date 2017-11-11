a1570123fa61f445f31a9ce99808a05394f61c3b
Jabari Dash <jabari.dash@gmail.com>
2017-11-10 19:18:11 -0500

    another attempt

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -1,16 +1,39 @@
 echo "Pushing to Dockerhub..."
 
+docker images
+
+# Get the branch that
+# the travis script
+# is currently being run from
 branch=$TRAVIS_BRANCH
 
+# If we are in the master branch
 if [ "$branch" == "master" ]; then
   echo "Pushing to $branch branch"
   docker push jabaridash/personal_website:latest
 
+  # If the previous command was
+  # not successful, then the whole
+  # script failed
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+
+# If we are in the dev branch
 elif [ "$branch" == "dev" ]; then
   echo "Pushing to $branch branch"
   docker push jabaridash/personal_website-dev:latest
 
+  # If the previous command was
+  # not successful, then the whole
+  # script failed
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+
+# If the branch is not dev or master
 else
   echo "branch '$branch' is not supported"
+  exit 1
 
 fi
