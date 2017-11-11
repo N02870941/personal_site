003af9af6acb7570deee0e97b914e4c9bc9c26ac
Jabari Dash <jabari.dash@gmail.com>
2017-11-10 19:09:21 -0500

    try to push to docker

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -4,9 +4,11 @@ branch=$TRAVIS_BRANCH
 
 if [ "$branch" == "master" ]; then
   echo "Pushing to $branch branch"
+  docker push jabaridash/personal_website:latest
 
 elif [ "$branch" == "dev" ]; then
   echo "Pushing to $branch branch"
+  docker push jabaridash/personal_website-dev:latest
 
 else
   echo "branch '$branch' is not supported"
