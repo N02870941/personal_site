f2f470f8dc6017727a5fbfa0a9c1102019d69360
Jabari Dash <jabari.dash@gmail.com>
2017-11-25 14:12:29 -0500

    reminify css

--- src/gulp/styles.js
+++ src/gulp/styles.js
@@ -5,10 +5,10 @@ var minify = require('gulp-minify-css');
 module.exports = function (gulp, plugins) {
   return function() {
 
-    // Concat all css
+    // Concat all css and minify it
     return gulp.src('./client/core/style/**/*.css')
       .pipe(concat('styles.min.css'))
-      // .pipe(minify())
+      .pipe(minify())
       .pipe(gulp.dest('./dist'));
   }
 };
