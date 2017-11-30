9fafc68eeb7aaf72089204b535be86d3481fed9b
Jabari Dash <jabari.dash@gmail.com>
2017-11-30 03:37:38 -0500

    added fav icon, made more variables in scss

--- src/client/pages/home/home.component.js
+++ src/client/pages/home/home.component.js
@@ -7,7 +7,7 @@ app.component('myHome', {
       "I am ambidextrous",
       "I'm a Software Engineer who loves to code",
       "An (aspiring) World traveler",
-      "I lived in Ecuador for 5 months"
+      "I lived in Ecuador for 5 months",
       "I skip leg day and I don't care",
       "Struggling to be vegetarian"
     ];
