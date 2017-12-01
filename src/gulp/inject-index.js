ced16feab67ef71026b6de90a34c5a625e9f9907
Jabari Dash <jabari.dash@gmail.com>
2017-12-01 16:29:50 -0500

    tured everything into immediately invoked function expressions (IIFE), not concatenating JS for dev purposes

--- src/gulp/inject-index.js
+++ src/gulp/inject-index.js
@@ -4,8 +4,13 @@ module.exports = function (gulp, plugins) {
 
   return function() {
     // Inject js and css dependencies into index.html
+    // return gulp.src('./index.html')
+    //     .pipe(inject(gulp.src('./dist/**/*min.{js,css}', {read : false}), {relative: true}))
+    //     .pipe(gulp.dest('./'));
+
+    // NOTE - TEMPORARY
     return gulp.src('./index.html')
-        .pipe(inject(gulp.src('./dist/**/*min.{js,css}', {read : false}), {relative: true}))
+        .pipe(inject(gulp.src('./dist/**/*.{js,css}', {read : false}), {relative: true}))
         .pipe(gulp.dest('./'));
   }
 };
