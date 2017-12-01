00f51d5e82fd2e0b43ea7c56e5266e9250bfb978
Jabari Dash <jabari.dash@gmail.com>
2017-12-01 02:21:16 -0500

    rogue domain fix

--- src/client/core/app/app.module.js
+++ src/client/core/app/app.module.js
@@ -1,3 +1,9 @@
+
+// NOTE - This is the only global function
+// in regular JavaScript scope. This allows
+// content from the config file to be accessed
+// from anywhere within the application, independent
+// of AngularJS rules, modules, etc
 function getConfig() {
   return JSON.parse(
     $.ajax({
@@ -8,9 +14,12 @@ function getConfig() {
   );
 }
 
+//------------------------------------------------------------------------------
+
 var config = getConfig();
 var modules = config.modules;
 var dependencies = config.dependencies;
+
 //------------------------------------------------------------------------------
 
 // Create the module
