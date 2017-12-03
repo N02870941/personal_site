7da6dd8250b73bdcd24922a43c32afed2c47f0a2
Jabari Dash <jabari.dash@gmail.com>
2017-12-03 15:48:14 -0500

    got the modal working quite nicely

--- src/client/core/components/modal/modal.component.js
+++ src/client/core/components/modal/modal.component.js
@@ -10,12 +10,13 @@
       css: "client/core/components/modal/modal.css",
       controllerAs: "vm",
 
-      controller: function($location, $window) {
+      controller: function($window) {
         var vm = this;
         var modalImgIndex = 0;
-        var modalImgId = 'img01';
+        var modalImgId = 'modalImg';
         var modalId = 'myModal';
 
+// These functions are meant to be called only within the controller
 //------------------------------------------------------------------------------
 
         function updateModalImg(index) {
@@ -24,41 +25,10 @@
 
 //------------------------------------------------------------------------------
 
-        this.previous = function() {
-          modalImgIndex--;
-
-          if (modalImgIndex < 0) {
-            modalImgIndex = this.images.length-1;
-          }
-
-          updateModalImg(modalImgIndex);
-        }
-
-//------------------------------------------------------------------------------
-
-        this.next = function() {
-          modalImgIndex++;
-
-          if (modalImgIndex >= this.images.length) {
-            modalImgIndex = 0;
-          }
-
-          updateModalImg(modalImgIndex);
-        }
-
-//------------------------------------------------------------------------------
-
-        imgOnClick = function(imgId) {
-          var img = document.getElementById(imgId);
-          var modal = document.getElementById(modalId);
-          var modalImg = document.getElementById(modalImgId);
-
-          modal.style.display = "block";
-          modalImg.src = img.src;
-        }
-
-//------------------------------------------------------------------------------
-
+        // TODO - Get rid of these functions because
+        // they set up global key listeners that still
+        // exist when the app is in other states / pages
+        // Destroy them in $onDestroy() lifecycle hook
         function setupNextPrevArrowButtons() {
           $(document).keyup(function(e) {
             if (e.keyCode == 37) { // left arrow key maps to keycode `37`
@@ -75,68 +45,114 @@
 
 //------------------------------------------------------------------------------
 
-        function setupButton(keycode, action) {
+        function setupKeyListener(keycode, action) {
           $(document).keyup(function(e) {
-            if (e.keyCode == keycode) {
+            if (e.keyCode == keycode) { // escape key maps to keycode `27`
               action();
             }
           });
         }
 
-
 //------------------------------------------------------------------------------
 
-        function setupCloseButton(modal) {
-          // Get the <span> element that closes the modal
-          var span = document.getElementsByClassName("close")[0];
-
-          // When the user clicks on <span> (x), close the modal
-          span.onclick = function() {
-              modal.style.display = "none";
-          }
-        }
-
-//------------------------------------------------------------------------------
-
-        function setupCloseOnEscape(modal) {
-          // If the user clicks escape
-          $(document).keyup(function(e) {
-            if (e.keyCode == 27) { // escape key maps to keycode `27`
-              modal.style.display = "none";
-            }
-          });
-        }
-
-//------------------------------------------------------------------------------
-
+        /**
+         * @description Returns the HTML for an img
+         * @param imgID The designated image id
+         */
         function getImageDiv(imgID) {
-          return `
-            <div>
-              <img onclick='imgOnClick(this.id)'
-                   id="`+ imgID + `"
-                   class="grey-out"
-                   src="` + imgID + `">
-            </div>
-          `
+          var html = `
+            <img
+              onclick='imgOnClick(this.id)'
+              id="`+ imgID + `"
+              class="grey-out"
+              src="` + imgID + `">
+          `;
+
+          return html;
+        }
+
+// The following functions are exposed to HTML template via vm.function() notation
+//------------------------------------------------------------------------------
+
+        /**
+         * @description Navigates to the previous
+         * photo in the photo modal slideshow
+         */
+        vm.previous = function() {
+          modalImgIndex--;
+
+          if (modalImgIndex < 0) {
+            modalImgIndex = this.images.length-1;
+          }
+
+          updateModalImg(modalImgIndex);
         }
 
 //------------------------------------------------------------------------------
 
-        this.$onInit = function() {
+        /**
+         * @description Navigates to the next photo in
+         * the photo modal slideshow
+         */
+        vm.next = function() {
+          modalImgIndex++;
+
+          if (modalImgIndex >= this.images.length) {
+            modalImgIndex = 0;
+          }
+
+          updateModalImg(modalImgIndex);
+        }
+
+//------------------------------------------------------------------------------
+
+        /**
+         * @description Action that gets executed when an image is clicked
+         * on from the main section. The image that is clicked on is assigned
+         * to the modalImg, and the modal opens the image in a larger view
+         *
+         * @param imgID The id of the img that was clicked on
+         */
+        vm.imgOnClick = function(imgId) {
+          var img      = document.getElementById(imgId);
+          var modalImg = document.getElementById(modalImgId);
+          var modal    = document.getElementById(modalId);
+
+          modal.style.display = "block";
+          modalImg.src = img.src;
+        }
+
+        // Expose function to JavaScript scope,
+        // Not sure why ng-click does not work
+        // with vm.imgOnClick()
+        imgOnClick = vm.imgOnClick;
+
+//------------------------------------------------------------------------------
+
+        /**
+         * @description Closes the modal view
+         */
+        vm.closeModal = function() {
+          document.getElementById(modalId).style.display = "none";
+        }
+
+//------------------------------------------------------------------------------
+
+        /**
+         * Sets up the modal when the component is initialized
+         */
+        vm.$onInit = function() {
 
           // TODO - figure out why the array
           // is empty when this state is loaded
           // from another state
           if (this.images.length == 0) {
             $window.location.reload();
-            console.log('page reloaded');
           }
 
           var photos = document.getElementById('photos');
-          var modal = document.getElementById('myModal');
 
-          setupCloseButton(modal);
-          setupCloseOnEscape(modal);
+          setupKeyListener(27, vm.closeModal);
           setupNextPrevArrowButtons();
 
           // Create the HTML for each image
