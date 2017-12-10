d83a8205668f047c029f6358ad46bfd0466143fa
Jabari Dash <jabari.dash@gmail.com>
2017-12-10 03:06:55 -0500

    injects css inline as well, inline injection is complete

--- src/gulp/index-inject-dependencies-internal-inline.js
+++ src/gulp/index-inject-dependencies-internal-inline.js
@@ -1,11 +1,14 @@
-var inject        = require('gulp-inject');
 var injectScripts = require('gulp-inject-scripts');
+var inlinesource  = require('gulp-inline-source');
 var gulpConfig    = require('../config/gulp/gulp.config.json');
 
 module.exports = function (gulp, plugins) {
   return function() {
 
+    // inject anything with inline tag (css)
+    // inject the scripts
     return gulp.src(gulpConfig.indexPath)
+      .pipe(inlinesource())
       .pipe(injectScripts({baseDir: gulpConfig.baseDir}))
       .pipe(gulp.dest(gulpConfig.baseDir));
   }
